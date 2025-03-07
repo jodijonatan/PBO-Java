2783307c32ba4edbd874732a272344125eaffe42
jodijonatan <jdi102350@gmail.com>
2025-03-07 09:20:08 +0700

    perubahan di file java

--- perkalian/Kali.java
+++ perkalian/Kali.java
@@ -1,24 +1,20 @@
-package pembagian;
+package perkalian;
 import java.util.Scanner;
 
-public class Bagi {
+public class Kali {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("== PROGRAM PEMBAGIAN SEDERHANA ==");
+        System.out.println("== PROGRAM PERKALIAN SEDERHANA ==");
         try {
             System.out.println("Masukkan angka pertama: ");
             double angka1 = scanner.nextDouble();
 
             System.out.println("Masukkan angka kedua: ");
             double angka2 = scanner.nextDouble();
-            if (angka2 == 0) {
-                System.err.println("Error: Pembagian dengan nol tidak diperbolehkan");
-                return;
-            } else {
-                double hasil = angka1 / angka2;
-                System.out.println("Hasil pembagian adalah: " + hasil);
-            }
+            
+            double hasil = angka1 * angka2;
+            System.out.println("Hasil perkalian adalah: " + hasil);
         } catch (Exception e) {
             System.err.println("Error: Masukkan angka yang valid");
         } finally {
