3b9383b659c442ff1ae0460781c113433c1eda89
jodijonatan <jdi102350@gmail.com>
2025-03-14 15:19:42 +0700

    fix

--- pembagian/Bagi.java
+++ pembagian/Bagi.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 
 public class Bagi {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner (System.in);
         System.out.println("== PROGRAM PEMBAGIAN SEDERHANA ==");
         try {
             System.out.println("Masukkan angka pertama: ");
             double angka1 = scanner.nextDouble();
 
-            System.out.println("Masukkan angka kedua: ");
+            System.out.println("Masukkan angka kedua");
             double angka2 = scanner.nextDouble();
-            if (angka2 == 0) {
-                System.err.println("Error: Pembagian dengan nol tidak diperbolehkan");
-                return;
-            } else {
+            if (angka2 != 0) {
                 double hasil = angka1 / angka2;
-                System.out.println("Hasil pembagian adalah: " + hasil);
-            }
-        } catch (Exception e) {
-            System.err.println("Error: Masukkan angka yang valid");
+                System.out.println("Hasilnya adalah: " + hasil);
+            } else {
+                System.out.println("Pembagian dengan nol tidak diperbolehkan");
+                return;
+            } 
+        }  catch (Exception e) {
+            System.out.println("Masukkan angka yang valid");
         } finally {
-            scanner.close();
+            close.scanner();
         }
-    }
-}
+    } 
+} 
