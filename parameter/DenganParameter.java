81498ae92e61a9504f38ebd6ad806abcc10f454a
Jodi Jonatan <jdi102350@gmail.com>
2025-05-14 09:53:32 +0700

    first commit in windows

--- parameter/DenganParameter.java
+++ parameter/DenganParameter.java
@@ -2,7 +2,7 @@ package parameter;
 
 public class DenganParameter {
     public static void sapa(String nama) {
-        System.out.println("Halo " + nama + ", CIHUYY");
+        System.out.println("Halo " + nama + ", nice to meet you!");
     }
 
     public static void main(String[] args) {
