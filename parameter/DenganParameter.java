19dd1a46c438cb5c7cc47d9381a1b45dc43cc1e9
Jodi Jonatan <jdi102350@gmail.com>
2025-05-14 14:28:15 +0700

    commit

--- parameter/DenganParameter.java
+++ parameter/DenganParameter.java
@@ -4,6 +4,7 @@ public class DenganParameter {
     public static void sapa(String nama) {
         System.out.println("Halo " + nama + ", nice to meet you");
         System.out.println("Halo " + nama + ", CIHUYY");
+        System.out.println("Halo " + nama + ", nice to meet you!");
     }
 
     public static void main(String[] args) {
